f11a66457229e94711d8701499f1a357ca7e5fd2
yuunoza <yuuki060127@gmail.com>
2017-02-24 13:31:11 +0900

    cheese通った

--- AOJ/cheese_0558/main.cpp
+++ AOJ/cheese_0558/main.cpp
@@ -1,37 +1,53 @@
 #include <iostream>
 #include <queue>
+#include <vector>
 
 
 using namespace std;
 
 const int MAX_H = 1002;
 const int MAX_W = 1002;
-char map[MAX_H][MAX_W];
+vector<vector <int> > map;
+vector<vector <int> > map_copy;
 
+int INF = 100000;
 int main() {
 
     int h, w, n;
-    queue< pair <pair<int, int>, pair<int, int> > >  q;
+    queue<vector<int>> q;
 
     cin >> h >> w >> n;
 
-    for(int i = 0; i < MAX_H; i++){
-        for (int j = 0; j < MAX_W; j++) {
-            map[i][j] = 'X';
+    map.resize(h+2);
+    map_copy.resize(h+2);
+
+    for (int i = 0; i < h + 2; i++) {
+        map[i].resize(w+2);
+        map_copy[i].resize(w+2);
+    }
+    for(int i = 0; i < h + 2; i++){
+        for (int j = 0; j < w + 2; j++) {
+            map[i][j] = -1;
         }
     }
 
+    char input_chr;
     for (int i = 1; i < h + 1; i++) {
         for (int j = 1; j < w + 1; j++) {
-            cin >> map[i][j];
-            if(map[i][j] == 'S'){
-                pair<int, int> start_pos;
-                start_pos.first = i;
-                start_pos.second = j;
-                pair<int, int> init_counter_hp;
-                init_counter_hp.first = 0;
-                init_counter_hp.second = 1;
-                q.push( pair<pair<int, int>, pair<int, int> >(start_pos, init_counter_hp));
+            cin >> input_chr;
+            switch (input_chr){
+                case 'S':
+                    q.push({i, j, 0});
+                    map[i][j] = 0;
+                    break;
+                case '.':
+                    map[i][j] = 0;
+                    break;
+                case 'X':
+                    map[i][j] = -1;
+                    break;
+                default:
+                    map[i][j] = int(input_chr - '0');
             }
         }
     }
@@ -39,39 +55,32 @@ int main() {
     int mh[4] = {1,0,0,-1};
     int mw[4] = {0,1,-1,0};
 
-    pair<pair<int, int>, pair<int, int> > now_pos_counter;
-    int cheese;
+    vector<int> h_w_count;
+    h_w_count.resize(3);
+    map_copy = map;
 
-    while(!q.empty()){
-        now_pos_counter = q.front();
-        q.pop();
-        if(map[now_pos_counter.first.first][now_pos_counter.first.second] != '.'){
-            cheese = int(map[now_pos_counter.first.first][now_pos_counter.first.second] - '0');
-            if(cheese <= now_pos_counter.second.second ){
-                if(cheese == n) {
-                    break;
-                }
-                now_pos_counter.second.second += 1;
-                map[now_pos_counter.first.first][now_pos_counter.first.second] = '.';
-                while(!q.empty()) q.pop();
-                //現在地入れてcontinue;
+    for(int i = 1; i < n+1; i++){
+        while(!q.empty()) {
+            h_w_count = q.front();
+            q.pop();
+            if(map[h_w_count[0]][h_w_count[1]] == i){
+                while (!q.empty()) q.pop();
+                q.push({h_w_count[0], h_w_count[1], h_w_count[2]});
+                map = map_copy;
+                break;
             }
-        }
-        for (int i = 0; i < 4; i++) {
-            if(map[now_pos_counter.first.first + mh[i]][now_pos_counter.first.second + mw[i]] != 'X'){
-                pair<int, int> next_pos;
-                next_pos.first = now_pos_counter.first.first + mh[i];
-                next_pos.second = now_pos_counter.first.second + mw[i];
-                pair<int ,int> next_counter_pos;
-                next_counter_pos.first = now_pos_counter.second.first + 1;
-                next_counter_pos.second = now_pos_counter.second.second;
-                q.push(pair<pair<int, int>, pair<int,int> >(next_pos, next_counter_pos));
-
+            map[h_w_count[0]][h_w_count[1]] = -1;
+            for (int j = 0; j < 4; j++) {
+                if(map[h_w_count[0] + mh[j]][h_w_count[1] + mw[j]] >= 0){
+                    q.push({h_w_count[0]+mh[j], h_w_count[1]+mw[j], h_w_count[2] + 1});
+                    if(map[h_w_count[0] + mh[j]][h_w_count[1] + mw[j]] == 0){
+                        map[h_w_count[0] + mh[j]][h_w_count[1] + mw[j]] = -1;
+                    }
+                }
             }
         }
     }
-    cout << now_pos_counter.second.first << endl;
+    cout << h_w_count[2] << endl;
     return 0;
 }
 
-//もう一個マップを持っておいてチーズ食べたら更新
