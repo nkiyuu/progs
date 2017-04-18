cb349b43cb64c0679328b2ce4e33e2bb30e0a957
Yuki Nozaki <nq127main@gmail.com>
2017-04-19 01:55:05 +0900

    srm712 問題2解けた

--- TopCoder/srm712_20170418/problem2/main.cpp
+++ TopCoder/srm712_20170418/problem2/main.cpp
@@ -28,15 +28,24 @@ int main() {
         if(input_string[i] != ""){
             plus_alpha = input_string[i];
             input_string[i] = "";
+            break;
         }
     }
     string rev_pali = "";
-    for(int i = (int)palindrome.size(); i == 0; --i){
-        rev_pali += palindrome[i];
+    for(int i = (int)palindrome.size(); i >= 0; --i){
+        rev_pali += palindrome.substr(i, 1);
     }
 
     string ans1 = palindrome + plus_alpha + rev_pali;
-    cout << palindrome << ":" << plus_alpha << ":" << rev_pali << endl;
-    cout << ans1 << endl;
+    vector<string> ans_list;
+    ans_list.push_back(ans1);
+    for(int i=0; i < input_string.size();i++){
+        if(input_string[i] != ""){
+            ans_list.push_back(input_string[i]);
+        }
+    }
+    for(int i = 0; i < ans_list.size(); i++){
+        cout << ans_list[i] << endl;
+    }
     return 0;
 }
