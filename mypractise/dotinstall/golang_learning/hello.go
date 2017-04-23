defb25db5e219be010d3d6bb9dd886fc3514d71a
Yuki Nozaki <yuuki060127@gmail.com>
2017-04-24 01:17:06 +0900

    golang ちょっと進めた

--- mypractise/dotinstall/golang_learning/hello.go
+++ mypractise/dotinstall/golang_learning/hello.go
@@ -3,9 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	a := 5
-	var pa *int
-	pa = &a
-	fmt.Println(pa)
-	fmt.Println(*pa)
+
+	score := 83
+
+	if score > 80 {
+		fmt.Println("Great")
+	} else if score > 60 {
+		fmt.Println("Good!")
+	} else {
+		fmt.Println("so...so...")
+	}
 }
