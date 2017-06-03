bb7a62d9b57fb0983e2c9d82db3750c331eb83a9
Yuki Nozaki <yuuki060127@gmail.com>
2017-06-04 05:30:09 +0900

    complete tour-of-heroes
    とりあえず全部やった

--- prac_angular2/tour_of_heros/src/app/app.module.ts
+++ prac_angular2/tour_of_heros/src/app/app.module.ts
@@ -1,19 +1,37 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import {NgModule}      from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http'
 
-import { AppComponent }  from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import {AppComponent}  from './app.component';
+import {HeroesComponent} from './heroes.component'
+import {HeroDetailComponent} from './hero-detail.component';
+import {HeroService} from './hero.service';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
-  imports:      [
-      BrowserModule,
-      FormsModule
-   ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
+  ],
   declarations: [
-      AppComponent,
-      HeroDetailComponent
-    ],
-  bootstrap:    [ AppComponent ]
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
+  ],
+  providers: [HeroService],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
+
